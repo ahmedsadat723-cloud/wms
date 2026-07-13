d65b2bc89e89cfcd8062d941158648b468dd0ad6
Ahmed Sadat <ahmedsadat723@gmail.com>
2026-07-13 16:46:38 +0300

    Deploy latest WMS build

--- mobile/sw.js
+++ mobile/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sadat-depot-mobile-v20260713123523';
+const CACHE_NAME = 'sadat-depot-mobile-v20260713134635';
 const SHELL_FILES = ['./index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', function(e){
